1b58c7328dabc331200ebe7e495a86e235d6d997
Youness Abach <YOUNESS.ABACH1@ibm.com>
2025-03-08 12:06:29 +0000

    add logs

--- src/main/java/com/bezkoder/spring/swagger/service/FileService.java
+++ src/main/java/com/bezkoder/spring/swagger/service/FileService.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.io.File;
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -16,20 +18,23 @@ public class FileService {
 
 	@Value("${springdoc.path.file}")
 	private String filePath; // This will be injected from application.properties
+    private static final Logger logger = LoggerFactory.getLogger(FileService.class);
 
 
     public List<Product> readFile() {
         ObjectMapper objectMapper = new ObjectMapper();
         List<Product> products = null;
         String fullPath = filePath + File.separator + "products.json";
-
+        logger.info("Démarrage du traitement...");
+        logger.info("path file : {}", filePath);
         try {
             // Assuming the JSON file is named "products.json"
         	File file = new File(fullPath);
             Product[] productsArray = objectMapper.readValue(file, Product[].class);
             products = Arrays.asList(productsArray);
+            logger.debug("Traitement en cours...");
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.error("Erreur lors du traitement", e);
         }
 
         return products;
